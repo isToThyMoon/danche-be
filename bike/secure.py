930e6d94376cea8201be8bbca46110b87d03538d
ayrikiya <2916709093@qq.com>
2020-03-15 19:03:52 +0800

    config

--- bike/secure.py
+++ bike/secure.py
@@ -1,6 +1,6 @@
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:mysql0621@47.101.187.60:3306/bike'
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:ss0621@45.77.201.159:3306/danche-be'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = 'True'
 
